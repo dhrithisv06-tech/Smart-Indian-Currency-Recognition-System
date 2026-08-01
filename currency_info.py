02b65f3a00bd5c2590c31f88b843995d5ed65f3d
Dhrithi S V <dhrithi.sv06@gmail.com>
2026-08-01 16:09:53 +0530

    Add files via upload

---
+++ currency_info.py
@@ -0,0 +1,75 @@
+CURRENCY_INFO = {
+
+    "₹10 (New)": {
+        "Denomination": "₹10",
+        "Issued By": "Reserve Bank of India",
+        "Color": "Chocolate Brown",
+        "Series": "Mahatma Gandhi New Series",
+        "Status": "Valid Indian Currency"
+    },
+
+    "₹10 (Old)": {
+        "Denomination": "₹10",
+        "Issued By": "Reserve Bank of India",
+        "Color": "Brown",
+        "Series": "Old Series",
+        "Status": "Valid Indian Currency"
+    },
+
+    "₹20": {
+        "Denomination": "₹20",
+        "Issued By": "Reserve Bank of India",
+        "Color": "Greenish Yellow",
+        "Series": "Mahatma Gandhi New Series",
+        "Status": "Valid Indian Currency"
+    },
+
+    "₹50 (New)": {
+        "Denomination": "₹50",
+        "Issued By": "Reserve Bank of India",
+        "Color": "Fluorescent Blue",
+        "Series": "Mahatma Gandhi New Series",
+        "Status": "Valid Indian Currency"
+    },
+
+    "₹50 (Old)": {
+        "Denomination": "₹50",
+        "Issued By": "Reserve Bank of India",
+        "Color": "Blue",
+        "Series": "Old Series",
+        "Status": "Valid Indian Currency"
+    },
+
+    "₹100 (New)": {
+        "Denomination": "₹100",
+        "Issued By": "Reserve Bank of India",
+        "Color": "Lavender",
+        "Series": "Mahatma Gandhi New Series",
+        "Status": "Valid Indian Currency"
+    },
+
+    "₹100 (Old)": {
+        "Denomination": "₹100",
+        "Issued By": "Reserve Bank of India",
+        "Color": "Purple",
+        "Series": "Old Series",
+        "Status": "Valid Indian Currency"
+    },
+
+    "₹200": {
+        "Denomination": "₹200",
+        "Issued By": "Reserve Bank of India",
+        "Color": "Bright Yellow",
+        "Series": "Mahatma Gandhi New Series",
+        "Status": "Valid Indian Currency"
+    },
+
+    "₹500": {
+        "Denomination": "₹500",
+        "Issued By": "Reserve Bank of India",
+        "Color": "Stone Grey",
+        "Series": "Mahatma Gandhi New Series",
+        "Status": "Valid Indian Currency"
+    }
+
+}
---
+++ labels.py
@@ -0,0 +1,23 @@
+CLASS_NAMES = [
+    "INDIA100NEW",
+    "INDIA100OLD",
+    "INDIA10NEW",
+    "INDIA10OLD",
+    "INDIA20",
+    "INDIA200",
+    "INDIA500",
+    "INDIA50NEW",
+    "INDIA50OLD"
+]
+
+CURRENCY_LABELS = {
+    "INDIA10NEW": "₹10 (New)",
+    "INDIA10OLD": "₹10 (Old)",
+    "INDIA20": "₹20",
+    "INDIA50NEW": "₹50 (New)",
+    "INDIA50OLD": "₹50 (Old)",
+    "INDIA100NEW": "₹100 (New)",
+    "INDIA100OLD": "₹100 (Old)",
+    "INDIA200": "₹200",
+    "INDIA500": "₹500"
+}
