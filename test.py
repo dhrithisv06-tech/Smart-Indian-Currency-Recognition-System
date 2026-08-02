35dc131cac437b8049d81138e69c7bd3a7bcfc79
Dhrithi S V <dhrithi.sv06@gmail.com>
2026-08-02 12:08:05 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-from predict import predict_image
-
-image = Image.open("sample_images/test.jpg")
-
-prediction, confidence = predict_image(image)
-
-print("Prediction :", prediction)
-print(f"Confidence : {confidence:.2f}%")
